ecd41558531e6068d977bd6dae9415d9031d19dc
Taylor Henderson <tph5595@verizon.net>
2016-09-17 19:33:36 -0400

    updated help

--- App/spaceGeek/src/index.js
+++ App/spaceGeek/src/index.js
@@ -91,7 +91,7 @@ Fact.prototype.intentHandlers = {
     },
 
     "AMAZON.HelpIntent": function (intent, session, response) {
-        response.ask("You can say tell me a space fact, or, you can say exit... What can I help you with?", "What can I help you with?");
+        response.ask("Name the hackathon you wish to here the schedule for", "What schedule would you like?");
     },
 
     "AMAZON.StopIntent": function (intent, session, response) {
