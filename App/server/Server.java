e0befed895ae386fcaf8f08fb6a25cf36caf877b
Taylor Henderson <tph5595@verizon.net>
2016-09-17 19:49:37 -0400

    added exception handling

---
+++ App/server/Server.java
@@ -0,0 +1,18 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+public static void main(String[] args){
+        try{
+                ServerSocket server = new ServerSocket(80);
+
+                System.out.println("Server has started on 127.0.0.1:8090.\r\nWaiting for a connection...");
+
+                Socket client = server.accept();
+
+                System.out.println("A client connected.");
+        }catch(Exception e) {
+                System.out.println(e);
+        }
+}
+}
