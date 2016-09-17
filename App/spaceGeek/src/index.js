4e9c08b55269710ce8ce76ecb773e6da0e7c842f
Taylor Henderson <tph5595@verizon.net>
2016-09-17 18:43:59 -0400

    string change

--- App/spaceGeek/src/index.js
+++ App/spaceGeek/src/index.js
@@ -68,7 +68,7 @@ Fact.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest,
 
 Fact.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
   console.log("HelloWorld onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
-  var speechOutput = "Which hackathon would you like to hear about?";
+  var speechOutput = "Which mlh hackathon would you like to hear about?";
   var repromptText = "I said Loading Celery man";
   response.ask(speechOutput, speechOutput);
 };
