f37e0579a456272279d4cf01b6fdb5f20a31d064
Taylor Henderson <tph5595@verizon.net>
2016-09-18 04:25:00 -0400

    flashy auto setup

--- Regex/gen.sh
+++ Regex/gen.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-#!/bin/bash
-# Delete all containers
-docker rm $(docker ps -a -q)
-# Delete all images
-docker rmi $(docker images -q)
 docker build tph5595:v1 .
 rm index.h*
 rm *.class
