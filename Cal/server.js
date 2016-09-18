8f915be042034e9dc829598a6c4092ad2fbb6915
Taylor Henderson <tph5595@verizon.net>
2016-09-18 07:20:16 -0400

    DONE

--- Cal/server.js
+++ Cal/server.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
-
+const spawn = require('child_process').spawn;
 app.get('/', function(req, res) {
     res.send('Hello World!');
 });
 app.get('/p', function(req, res) {
-    res.send(req.query.name + "\t" + req.query.date + "\t" + req.query.time + "\t" + req.query.even);
+    const ls = spawn('python', ['gen.py', 'req.query.name', 'req.query.dt']);
+    res.send(req.query.name + "\t" + req.query.dt);
 });
 app.listen(8080, function() {
     console.log('Example app listening on port 8080!');
