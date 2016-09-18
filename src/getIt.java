272f5f075d14433533e162aba0f23a1efce75e21
Huy Ngo <weknow_910@verizon.net>
2016-09-18 00:08:16 -0400

    getIt initial commit
    does not handle url exceptions

--- src/getIt.java
+++ src/getIt.java
@@ -4,11 +4,92 @@
  * 2.	Find all hackathon URLs
  * 3.	Write all URLs to URLS.txt file to get parsed by Parse.java
  */
+import java.util.regex.*;
 public class getIt {
 
+	public static final String REGULAR_EXPRESSION = "(http|https)://.*(com|io|edu|org)";
+	public static Pattern pattern;
+	public static Matcher matcher;
+			
+	public getIt(String input)
+	{
+		this.input = input;
+	}
+	
+	public String nextToken()
+	{
+		if(input.length() == 0)
+			return null;
+		String temp = "";
+		pattern = Pattern.compile(REGULAR_EXPRESSION);
+		matcher = pattern.matcher(input);
+		if(matcher.lookingAt())
+		{
+			temp = matcher.group(0);
+			input = input.substring(matcher.end(),input.length());
+			return temp;
+		}
+		return null;
+	}
+			
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		StringBuilder inputBuilder = new StringBuilder();
+		File inFile = null;
+		String input = "";
+        String line = null;
+        String temp = "";
+        
+        File inFile = null;
+        if (0 < args.length)
+        {
+           inFile = new File(args[0]);
+        }
+        else
+        {
+           System.err.println("Invalid arguments count:" + args.length);
+           System.exit(0);
+        }	        
+        FileReader fileReader = new FileReader(inFile);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        try
+        {
+        	while((line = bufferedReader.readLine()) != null)
+        	{
+                inputBuilder.append(line);
+        	}
+            bufferedReader.close();
+            input = inputBuilder.toString();
+        }
+        catch (IOException e)
+        {
+        	e.printStackTrace();
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        getIt tokenizer = new getIt(input);
+        while(input!= null && input.length() > 0)
+        {
+        	input = tokenizer.nextToken();
+        	sb.append(input);
+        	sb.append("\n");
+        }
+        
+        File outFile = null;
+        if (0 < args.length)
+           outFile = new File(args[1]);
+        else
+        {
+           System.err.println("Invalid arguments count:" + args.length);
+           System.exit(0);
+        }	        
+        FileWriter fileWriter = new FileWriter(outFile);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        bufferedWriter.write(sb.toString());
+        bufferedWriter.close();
+        System.out.println(sb.toString());
+       
 	}
 
 }
