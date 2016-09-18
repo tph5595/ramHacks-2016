b4848310be482814d40bca9b294bbe0e21fafd60
Taylor Henderson <tph5595@verizon.net>
2016-09-18 06:45:33 -0400

    server node

---
+++ Cal/server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+});
+app.get('/p', function(req, res) {
+    res.send(req.query.name + "\t" + req.query.date + "\t" + req.query.time + "\t" + req.query.even);
+});
+app.listen(8080, function() {
+    console.log('Example app listening on port 3000!');
+});
