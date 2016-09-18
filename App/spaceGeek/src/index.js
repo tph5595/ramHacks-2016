3fcb2aead5042855efab3c4953486b36b6fe67d6
Chris Burns <burns.christopher13@gmail.com>
2016-09-18 04:35:52 -0400

    Removed some unused code

--- App/spaceGeek/src/index.js
+++ App/spaceGeek/src/index.js
@@ -203,19 +203,6 @@ Fact.prototype.intentHandlers = {
     }
 };
 
-/**
- * Gets a random new fact from the list and returns to the user.
- */
-function handleNewFactRequest(response) {
-    // Get a random space fact from the space facts list
-    var factIndex = Math.floor(Math.random() * FACTS.length);
-    var randomFact = FACTS[factIndex];
-
-    // Create speech output
-    var speechOutput = "Here's your fact: " + randomFact;
-    var cardTitle = "Your Fact";
-    response.tellWithCard(speechOutput, cardTitle, speechOutput);
-}
 
 function parseJson(inputText) {
     // sizeOf (/nEvents/n) is 10
