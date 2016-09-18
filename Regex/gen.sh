ed98f3e6cb10d74919fffe72ae46c4d94e774600
Taylor Henderson <taylor.henderson@telos.com>
2016-09-18 09:35:17 -0400

    correct gen file

--- Regex/gen.sh
+++ Regex/gen.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 docker build tph5595:v1 .
 rm index.h*
+rm *.txt
 rm *.class
 javac *.java
-java getIt
+java getIt The*.html
 for i in `cat urls.txt`
 do
 	wget $i
+	java Parse $i index.html
+	rm index.html
 done 
