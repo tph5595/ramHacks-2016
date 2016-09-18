0ad725ef7931fe867a34e471fcf65318180658d4
Taylor Henderson <tph5595@verizon.net>
2016-09-18 07:13:47 -0400

    i cant remeber

--- App/spaceGeek/src/index.js
+++ App/spaceGeek/src/index.js
@@ -109,6 +109,20 @@ var NAME_TO_FILE = {
     "hacknotts": "hacknotts"
 };
 
+var months = {
+    "january": "01",
+    "february": "02",
+    "march": "03",
+    "april": "04",
+    "may": "05",
+    "june": "06",
+    "july": "07",
+    "august": "08",
+    "september": "09",
+    "october": "10",
+    "november": "11",
+    "december": "12"
+};
 /**
  * The AlexaSkill prototype and helper functions
  */
