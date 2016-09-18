41e1fa77e899716b2068ce6639674bc6ba2834e3
Taylor Henderson <tph5595@verizon.net>
2016-09-17 21:15:12 -0400

    maybe

--- Regex/test.js
+++ Regex/test.js
@@ -16,7 +16,7 @@ https.get(url, function(res) {
         stringResult = parseJson(body);
         //eventCallback(stringResult);
     });
-    console.log(parseJson(body));
+    console.log(body);
 }).on('error', function(e) {
     console.log("Got error: ", e);
 });
