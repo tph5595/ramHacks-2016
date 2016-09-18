6d1472a0d60002c8265950620d4197b785a776c3
Taylor Henderson <tph5595@verizon.net>
2016-09-17 21:18:45 -0400

    http

--- Regex/test.js
+++ Regex/test.js
@@ -5,8 +5,8 @@ var delimiterSize = 2;
 var https = require('https');
 var http = require('http');
 var stringResult = "before";
-var url = "http://ramhacks.vcu.edu/";
-http.get(url, function(res) {
+var url = "https://ramhacks.vcu.edu/";
+https.get(url, function(res) {
     var body = '';
 
     res.on('data', function(chunk) {
