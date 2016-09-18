bb1a8434444a8a96eb5b71875c8d86770c15e792
Taylor Henderson <tph5595@verizon.net>
2016-09-18 06:45:57 -0400

    updated port

--- Cal/server.js
+++ Cal/server.js
@@ -8,5 +8,5 @@ app.get('/p', function(req, res) {
     res.send(req.query.name + "\t" + req.query.date + "\t" + req.query.time + "\t" + req.query.even);
 });
 app.listen(8080, function() {
-    console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 8080!');
 });
