d0e8d0cd14189494406f21981484080e6390003f
Eric <maverick17ev@comcast.net>
2016-09-17 23:09:38 -0400

    Add files via upload
    Open the files, stop, having them be closed.

---
+++ src/getIt.java
@@ -0,0 +1,14 @@
+/*
+ * This program will:
+ * 1.	Read https://mlh.io/seasons/na-2017/events
+ * 2.	Find all hackathon URLs
+ * 3.	Write all URLs to URLS.txt file to get parsed by Parse.java
+ */
+public class getIt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
